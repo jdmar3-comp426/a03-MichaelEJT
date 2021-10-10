ae3151bb00bc1e75173e2cd22c022724b8570e9b
MichaelEJT <Ejt20010427!@>
2021-10-10 19:33:17 -0400

    mild_1

--- test.js
+++ test.js
@@ -7,3 +7,6 @@ console.log(getIncreasingArray(3,7));
 import { maxAndMin } from "./src/mild/mild_1.js";
 console.log("Testing maxAndMin");
 console.log(maxAndMin([1,2,3,4,5,6,7,8,9,10]));
+import { countArray } from "./src/mild/mild_1.js";
+console.log("Testing countarray");
+console.log(countArray([1,1,1,3,3,3,5,4,5,4,5,4,5]))
