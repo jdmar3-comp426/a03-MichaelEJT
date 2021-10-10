60e8ab4c24391448d37495e205fcf599bb991a0f
MichaelEJT <Ejt20010427!@>
2021-10-10 19:31:00 -0400

    aaa

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 import {sumToString} from "./src/mild/mild_1.js";
 console.log("Testing sumToString")
 console.log(sumToString(3,4));
+import { getIncreasingArray } from "./src/mild/mild_1.js";
+console.log("Testing getIncreasingArray");
+console.log(getIncreasingArray(3,7));
+import { maxAndMin } from "./src/mild/mild_1.js";
+console.log("Testing maxAndMin");
+console.log(maxAndMin([1,2,3,4,5,6,7,8,9,10]));
