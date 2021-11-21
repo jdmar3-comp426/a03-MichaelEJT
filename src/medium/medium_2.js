c07f06d674b10011e3a624f6c4d1ae3614d16417
MichaelEJT <Ejt20010427!@>
2021-11-20 19:11:12 -0500

    aaaa

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -20,46 +20,30 @@ see under the methods section
  * @param {allCarStats.ratioHybrids} ratio of cars that are hybrids
  */
 export const allCarStats = {
-    avgMpg: function(arr) {
+    avgMpg: undefined,
+    allYearStats: undefined,
+    ratioHybrids: undefined,
+};
         const cityMpg = [];
         const hwyMpg = [];
-        for (let i=0; i<arr.length; i++) {
-            cityMpg.push(arr[i].city_mpg);
-        }
-        for (let i=0; i<arr.length; i++) {
-            hwyMpg.push(arr[i].highway_mpg);
-        }
+        const years = [];
+        let hybrids = 0;
+        mpg_data.forEach(car => {
+            cityMpg.push(car.city_mpg);
+            hwyMpg.push(car.highway_mpg);
+            years.push(car.year);
+            if (car.hybrid) {
+                hybrids++;
+            }
+        });
+        
         var hwyMpgAverage = getStatistics(hwyMpg).mean;
         var cityMpgAverage = getStatistics(cityMpg).mean;
-        const ret = {city: cityMpgAverage, highway:hwyMpgAverage};
-        return ret;
+        const mpg = {city: cityMpgAverage, highway:hwyMpgAverage};
+        allCarStats.avgMpg = ret;
+        allCarStats.allYearStats = getStatistics(years);
+        allCarStats.ratioHybrids = hybrids / mpg_data.length;
 
-    },
-    allYearStats: function(arr) {
-        const years = [];
-        for (let i=0; i<arr.length; i++) {
-            years.push(arr[i].year);
-        }
-        const yearStats = getStatistics(years);
-        return yearStats;
-    },
-    ratioHybrids: function(arr) {
-        const hyb = [];
-        for (let i=0; i<arr.length; i++) {
-            hyb.push(arr[i].hybrid);
-        }
-        let y = 0;
-        
-        for (let i=0; i<hyb.length; i++) {
-            if (hyb[i] == true) {
-                y++;
-            }
-        }
-        let ret = y / hyb.length;
-        return ret;
-    },
-    
-};
 
 
 /**
